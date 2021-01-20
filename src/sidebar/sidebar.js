c92121d405020c938c4c69ba9c5d1524a36a3afa
Roshini <roshbhaskar25@gmail.com>
2021-01-20 16:38:57 +0530

    First commit

---
+++ src/sidebar/sidebar.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+import List from '@material-ui/core/List';
+import { Divider, Button } from '@material-ui/core';
+import SidebarItemComponent from '../sidebaritem/sidebarItem';
+
+class SidebarComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      addingNote: false,
+      title: null,
+      inbox:true
+    };
+  }
+  
+  newNoteBtnClick = () => {
+    this.setState({ title: null, addingNote: !this.state.addingNote });
+  }
+  updateTitle = (txt) => {
+    this.setState({ title: txt });
+  }
+  newNote = () => {
+    this.props.newNote(this.state.title);
+    this.setState({ title: null, addingNote: false });
+  }
+  selectNote = (n, i) => this.props.selectNote(n, i);
+  deleteNote = (note) => this.props.deleteNote(note);
+  binNote = (note) => this.props.binNote(note);
+
+  render() {
+    const { notes, classes, selectedNoteIndex } = this.props;
+    if(notes) {
+    const binednotes = notes.filter(_note => _note.bin == true);
+    const realnotes = notes.filter(_note => _note.bin == false);
+    
+    
+      return(
+        
+        <div className={classes.sidebarContainer}>
+          <Button
+            onClick={this.newNoteBtnClick}
+            className={classes.newNoteBtn}>{this.state.addingNote ? 'Cancel' : 'New Note'}</Button>
+            
+            {
+              this.state.addingNote ? 
+              <div>
+                <input type='text'
+                  className={classes.newNoteInput}
+                  placeholder='Enter note title'
+                  onKeyUp={(e) => this.updateTitle(e.target.value)}>
+                </input>
+                <Button 
+                  className={classes.newNoteSubmitBtn}
+                  onClick={this.newNote}>Submit Note</Button>
+              </div> :
+              null
+            }
+            <Button onClick={()=> this.setState({inbox:true})}>All Notes</Button>
+            <Button onClick={()=> this.setState({inbox:false})}>Bin</Button>
+            <List>
+              { this.state.inbox ?
+                realnotes.map((_note, _index) => {
+                  return(
+                    <div key={_index}>
+                      <SidebarItemComponent
+                        _note={_note}
+                        _index={_index}
+                        selectedNoteIndex={selectedNoteIndex}
+                        selectNote={this.selectNote}
+                        deleteNote={this.deleteNote}>
+                      </SidebarItemComponent>
+                      <Divider></Divider>
+                    </div>
+                  )
+                }) :
+                binednotes.map((_note, _index) => {
+                  return(
+                    <div key={_index}>
+                      <SidebarItemComponent
+                        _note={_note}
+                        _index={_index}
+                        selectedNoteIndex={selectedNoteIndex}
+                        selectNote={this.selectNote}
+                        deleteNote={this.deleteNote}>
+                      </SidebarItemComponent>
+                      <Divider></Divider>
+                    </div>
+                  )
+                })
+              }
+            </List>
+        </div>
+      );
+    } else {
+      return(<div></div>);
+    }
+  }
+
+
+}
+
+export default withStyles(styles)(SidebarComponent);
